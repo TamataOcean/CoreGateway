60f8f7609fb14e8c589da31ed7ea2e4f36ff3099
Romain Tourte <rtourte@yahoo.fr>
2023-04-13 16:23:41 +0200

    Create deploy_Mosquitto.sh

---
+++ components/deploy_Mosquitto.sh
@@ -0,0 +1,5 @@
+echo "*************************************"
+echo "MOSQUITTO INSTALLATION ..."
+echo "*************************************"
+## INSTALL Mosquitto
+sudo apt install -y mosquitto mosquitto-clients 
