958d3bdc9f4eb4acbcdbf7bfdbe29a25cbf35512
Romain Tourte <rtourte@yahoo.fr>
2023-04-13 16:24:55 +0200

    Create deploy_NodeJS.sh

---
+++ components/deploy_NodeJS.sh
@@ -0,0 +1,6 @@
+echo "*************************************"
+echo "NODEJS INSTALLATION ..."
+echo "*************************************"
+## NODEJS
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash - &&
+sudo apt-get install -y nodejs
