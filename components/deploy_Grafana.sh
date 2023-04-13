76e495258ad058943018a0b17f2f5f66b5778849
Romain Tourte <rtourte@yahoo.fr>
2023-04-13 16:24:15 +0200

    Create deploy_Grafana.sh

---
+++ components/deploy_Grafana.sh
@@ -0,0 +1,11 @@
+echo "*************************************"
+echo "GRAFANA INSTALLATION ..."
+echo "*************************************"
+## GRAFANA
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add - &&
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list &&
+sudo apt-get update &&
+sudo apt-get install -y grafana &&
+sudo /bin/systemctl daemon-reload &&
+sudo /bin/systemctl enable grafana-server &&
+sudo /bin/systemctl start grafana-server
