17ae3379bfe632164f60be204e482f7091aca0f2
Romain Tourte <rtourte@yahoo.fr>
2023-04-13 16:29:13 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ echo "*************************************"
 echo "DEPLOYMENT BEGIN ..."
 echo "*************************************"
 
-./components/deploy_Mosquitto.sh &&
-./components/deploy_NodeJS.sh
-./components/deploy_Grafana.sh && 
-./components/deploy_InfluxDB.sh &&
+sudo ./components/deploy_Mosquitto.sh &&
+sudo ./components/deploy_NodeJS.sh
+sudo ./components/deploy_Grafana.sh && 
+sudo ./components/deploy_InfluxDB.sh &&
 
 echo "*************************************"
 echo "DEPLOY END ..."
