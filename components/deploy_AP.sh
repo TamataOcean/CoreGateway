4a1026305b524139d037a0e9a033d556b2555b90
DeGaMon <quentin.gchnk@gmail.com>
2023-04-14 10:53:10 +0200

    Update deploy_AP.sh

---
+++ components/deploy_AP.sh
@@ -0,0 +1,28 @@
+echo "*************************************"
+echo "ACCES POINT INSTALLATION ..."
+echo "*************************************"
+
+sudo apt-get install -y dnsmasq hostapd &&
+
+sudo cp wifi_raspi/hostapd.conf /etc/hostapd/hostapd.conf &&
+sudo cp wifi_raspi/interfaces /etc/network/interfaces &&
+sudo cp wifi_raspi/dnsmasq.conf /etc/dnsmasq.conf &&
+sudo cp wifi_raspi/sysctl.conf /etc/sysctl.conf &&
+sudo cp wifi_raspi/dhcpcd.conf /etc/dhcpcd.conf &&
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward" &&
+sleep 5 &&
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE &&
+sudo iptables -t nat -A POSTROUTING -o usb0 -j MASQUERADE &&
+sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT &&
+sudo iptables -A FORWARD -i usb0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT &&
+sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT &&
+sudo iptables -A FORWARD -i wlan0 -o usb0 -j ACCEPT &&
+sleep 5
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat" &&
+sudo cp /home/pi/OceanIsOpen/systools/wifi_raspi/rc.local /etc/rc.local &&
+sudo chmod +x  /etc/rc.local &&
+
+sudo systemctl unmask hostapd &&
+sudo systemctl enable hostapd #&&
+
+#sudo reboot now
