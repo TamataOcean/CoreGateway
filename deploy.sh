feb1acf0722578271bd388dd1a0f329ed9ef4ea4
DeGaMon <quentin.gchnk@gmail.com>
2023-04-14 15:10:13 +0200

    Add deploy_oioService to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,19 +6,20 @@ echo "*************************************"
 
 chmod u+x ./components/*.sh &&
 
-#sudo ./components/*.sh &&
 sudo ./components/deploy_Mosquitto.sh &&
-sudo ./components/deploy_NodeJS.sh
+sudo ./components/deploy_NodeJS.sh &&
 sudo ./components/deploy_Grafana.sh && 
 sudo ./components/deploy_InfluxDB.sh &&
+sudo ./components/deploy_oioService.sh &&
 sudo ./components/deploy_AP.sh &&
 
+sudo apt-get update && 
 sudo apt-get upgrade -y &&
-echo "Rebooting..."
-sudo reboot now
 
 echo "*************************************"
 echo "DEPLOY END ..."
 echo "*************************************"
+echo "### Rebooting..."
+sudo reboot now
 
 #### PACKAGE DEPLOY END ####
