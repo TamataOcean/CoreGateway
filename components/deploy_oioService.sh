7866f72ad36d0d7fe41d036e93c63fb7de5a469d
Romain Tourte <rtourte@yahoo.fr>
2023-04-20 16:18:07 +0200

    Update deploy_oioService.sh

--- components/deploy_oioService.sh
+++ components/deploy_oioService.sh
@@ -15,4 +15,4 @@ npm install &&
 #### ADD SERVICE OIO ( server.js )
 sudo cp /home/$USER/OceanIsOpen/systools/oio.service /etc/systemd/system/oio.service &&
 sudo systemctl enable oio.service &&
-sudo systemctl start oio.service &&
+sudo systemctl start oio.service
