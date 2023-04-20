40dae80c4a0a6fec84657099f105a37e9f5e9925
Romain Tourte <rtourte@yahoo.fr>
2023-04-20 16:29:01 +0200

    Update deploy_NodeJS.sh

--- components/deploy_NodeJS.sh
+++ components/deploy_NodeJS.sh
@@ -5,4 +5,4 @@ echo "*************************************"
 #curl -sL https://deb.nodesource.com/setup_16.x | sudo bash - &&
 ## USING Node Version 10.22.1 for OceanIsOpen app
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash - &&
-sudo apt-get install -y nodejs npm
+sudo apt-get install -y nodejs npm && exit
