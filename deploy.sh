5e006bc7144d912f253a0ab4d75bf549a6afd2ba
Romain Tourte <rtourte@yahoo.fr>
2023-04-20 15:56:16 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ echo "*************************************"
 
 chmod u+x ./components/*.sh &&
 
-sudo ./components/deploy_Mosquitto.sh &&
 sudo ./components/deploy_NodeJS.sh &&
-#sudo ./components/deploy_Grafana.sh && 
-sudo ./components/deploy_InfluxDB.sh &&
-#sudo ./components/deploy_oioService.sh &&
-#sudo ./components/deploy_AP.sh &&
+sudo ./components/deploy_oioService.sh &&
 
+#sudo ./components/deploy_InfluxDB.sh &&
+#sudo ./components/deploy_Mosquitto.sh &&
+#sudo ./components/deploy_Grafana.sh && 
+#sudo ./components/deploy_AP.sh &&
 #sudo apt-get update && 
 #sudo apt-get upgrade -y &&
 
