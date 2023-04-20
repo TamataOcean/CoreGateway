7f6e227bb0eb5780074c5eecf796400ae5287882
DeGaMon <110177171+DeGaMon@users.noreply.github.com>
2023-04-20 11:17:11 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,17 @@ chmod u+x ./components/*.sh &&
 
 sudo ./components/deploy_Mosquitto.sh &&
 sudo ./components/deploy_NodeJS.sh &&
-sudo ./components/deploy_Grafana.sh && 
+#sudo ./components/deploy_Grafana.sh && 
 sudo ./components/deploy_InfluxDB.sh &&
-sudo ./components/deploy_AP.sh &&
+#sudo ./components/deploy_AP.sh &&
 
-sudo apt-get update && 
-sudo apt-get upgrade -y &&
+#sudo apt-get update && 
+#sudo apt-get upgrade -y &&
 
 echo "*************************************"
 echo "DEPLOY END ..."
 echo "*************************************"
-echo "### Rebooting..."
-sudo reboot now
+#echo "### Rebooting..."
+#sudo reboot now
 
 #### PACKAGE DEPLOY END ####
