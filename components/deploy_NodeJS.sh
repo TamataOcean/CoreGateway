906d088bf5eb50d17fe7dafa326d4d96e2320b64
Romain Tourte <rtourte@yahoo.fr>
2023-04-20 15:49:04 +0200

    Update node version 10.22.1

--- components/deploy_NodeJS.sh
+++ components/deploy_NodeJS.sh
@@ -2,5 +2,7 @@ echo "*************************************"
 echo "NODEJS INSTALLATION ..."
 echo "*************************************"
 ## NODEJS
-curl -sL https://deb.nodesource.com/setup_16.x | sudo bash - &&
+#curl -sL https://deb.nodesource.com/setup_16.x | sudo bash - &&
+## USING Node Version 10.22.1 for OceanIsOpen app
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash - &&
 sudo apt-get install -y nodejs
