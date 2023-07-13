ac3fb874c0950a5a43ad187c41c6f504adc80027
Romain Tourte <rtourte@yahoo.fr>
2023-07-13 22:56:32 +0200

    Update for RASPI OS (64Bit)

--- components/deploy_Postgres.sh
+++ components/deploy_Postgres.sh
@@ -2,10 +2,10 @@ echo "*************************************"
 echo "POSTGRES INSTALLATION ..."
 echo "*************************************"
 
-sudo apt install postgresql-14 postgresql-14-postgis-3 postgresql-14-cron postgresql-14-postgis-3-scripts postgresql-plpython3-14
+sudo apt install postgresql-13 postgresql-13-postgis-3 postgresql-13-cron postgresql-13-postgis-3-scripts postgresql-plpython3-13
 echo "deb https://packagecloud.io/timescale/timescaledb/debian/ $(lsb_release -c -s) main" | sudo tee /etc/apt/sources.list.d/timescaledb.list
 wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | sudo apt-key add -
 sudo apt-get update
-sudo apt install timescaledb-2-postgresql-14
+sudo apt install timescaledb-2-postgresql-13
 
 # NEXT STEPS SEE : https://github.com/jancelin/MultiProbeCase/blob/dev/gateway/MPC_install_postgresql_grafana_rfcommBT_rtk_.md
