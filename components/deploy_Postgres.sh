ca3da7677b5b7374d029bf331e6d905f29d69e34
Romain Tourte <rtourte@yahoo.fr>
2023-07-13 21:57:34 +0200

    Update deploy_Postgres from MPC gateway

--- components/deploy_Postgres.sh
+++ components/deploy_Postgres.sh
@@ -1,4 +1,11 @@
 echo "*************************************"
 echo "POSTGRES INSTALLATION ..."
 echo "*************************************"
-sudo apt-get install -y postgresql
+
+sudo apt install postgresql-14 postgresql-14-postgis-3 postgresql-14-cron postgresql-14-postgis-3-scripts postgresql-plpython3-14
+echo "deb https://packagecloud.io/timescale/timescaledb/debian/ $(lsb_release -c -s) main" | sudo tee /etc/apt/sources.list.d/timescaledb.list
+wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | sudo apt-key add -
+sudo apt-get update
+sudo apt install timescaledb-2-postgresql-14
+
+# NEXT STEPS SEE : https://github.com/jancelin/MultiProbeCase/blob/dev/gateway/MPC_install_postgresql_grafana_rfcommBT_rtk_.md
