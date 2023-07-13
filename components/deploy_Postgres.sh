04f01d1756fabe1c3aa085bfc526a2a9c4b349a4
Romain Tourte <rtourte@yahoo.fr>
2023-07-13 23:33:02 +0200

    Update deploy_Postgres.sh

--- components/deploy_Postgres.sh
+++ components/deploy_Postgres.sh
@@ -2,10 +2,18 @@ echo "*************************************"
 echo "POSTGRES INSTALLATION ..."
 echo "*************************************"
 
-sudo apt install postgresql-13 postgresql-13-postgis-3 postgresql-13-cron postgresql-13-postgis-3-scripts postgresql-plpython3-13
+sudo apt install -y postgresql-13 postgresql-13-postgis-3 postgresql-13-cron postgresql-13-postgis-3-scripts postgresql-plpython3-13
 echo "deb https://packagecloud.io/timescale/timescaledb/debian/ $(lsb_release -c -s) main" | sudo tee /etc/apt/sources.list.d/timescaledb.list
 wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | sudo apt-key add -
 sudo apt-get update
-sudo apt install timescaledb-2-postgresql-13
+sudo apt install -y timescaledb-2-postgresql-13
+
+echo "Update config file"
+sudo echo "listen_addresses = '*' \n shared_preload_libraries = 'timescaledb,pg_cron' \n cron.database_name = 'mpc'" | sudo tee -a /etc/postgresql/13/main/postgresql.conf
+sudo echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a /etc/postgresql/13/main/pg_hba.conf
+
+echo "Restarting service"
+systemctl restart postgresql
+
 
 # NEXT STEPS SEE : https://github.com/jancelin/MultiProbeCase/blob/dev/gateway/MPC_install_postgresql_grafana_rfcommBT_rtk_.md
